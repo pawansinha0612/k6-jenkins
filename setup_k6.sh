259993a80a1fa58af2cf522c293227c398f818b0
Pawan <pawansinha4u@gmail.com>
2020-09-26 22:05:56 +1000

    aa

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-sudo brew update
-sudo brew install dirmngr --install-recommends
-sudo brew adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
-echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
-sudo brew update
+# sudo brew update
+# sudo brew install dirmngr --install-recommends
+# sudo brew adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+# echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
+# sudo brew update
 sudo brew install k6
