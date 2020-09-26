584b32f1c5c51c6f5ca7ebdb001cb71e37f5c6ea
Pawan <pawansinha4u@gmail.com>
2020-09-26 22:24:09 +1000

    aa

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-apt-get update
-apt-get install dirmngr --install-recommends
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+sudo apt-get update
+sudo apt-get install dirmngr --install-recommends
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
-apt-get update
-apt-get install k6
+sudo apt-get update
+sudo apt-get install k6
