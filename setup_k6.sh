d16a3f877d70020506d07db67eadc3d7af8bf891
Pawan <pawansinha4u@gmail.com>
2020-09-26 22:08:20 +1000

    aa

--- setup_k6.sh
+++ setup_k6.sh
@@ -6,4 +6,4 @@ set -ex
 # sudo brew adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 # echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
 # sudo brew update
-sudo brew install k6
+brew install k6
